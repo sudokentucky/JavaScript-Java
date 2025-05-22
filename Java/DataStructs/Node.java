944060623ec6877ff3e4ac2a39faee9c8fd53e74
sudokentucky <3116549240704@ingenieria.usac.edu.gt>
2025-05-22 12:47:11 -0600

    Nodo

---
+++ Java/DataStructs/Node.java
@@ -0,0 +1,13 @@
+package Java.DataStructs;
+
+public class Node {
+    int data;
+    Node next;
+
+    public Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+    
+    
+}
